7d7a3df26c422c2db1d253f9c7c38d03d9275f29
Juan Pablo Rodriguez <jprodriguez@dcathenea.local>
2023-01-11 12:53:26 -0500

    Pagina de proyecto institucional terminada

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 let elemento = document.getElementById("menu");
-let contenedor=document.getElementById("main_container")
+let contenedor = document.getElementById("main_container")
 
 
-function activar(){
-    if(elemento.checked==true){
-     contenedor.classList.add("space");
-    }else{
+function desactivar() {
+    elemento.checked = false
+}
+
+function activar() {
+    if (elemento.checked == true) {
+        contenedor.classList.add("space");
+    } else {
         contenedor.classList.remove("space");
     }
 }
@@ -13,25 +17,25 @@ function activar(){
 let slideIndex = 1;
 showSlides(slideIndex)
 
-function plusSlides(n){
+function plusSlides(n) {
     showSlides(slideIndex += n)
 }
-function currentSlide(n){
+function currentSlide(n) {
     showSlides(slideIndex = n)
 }
-function showSlides(n){
+function showSlides(n) {
     let i;
     let slides = document.querySelectorAll(".main_mySlides");
-    let quadrates = document.querySelectorAll(".quadrate"); 
-    if(n > slides.length) slideIndex = 1
-    if(n < 1) slideIndex = slides.length
-    for(i = 0; i < slides.length; i++){
+    let quadrates = document.querySelectorAll(".quadrate");
+    if (n > slides.length) slideIndex = 1
+    if (n < 1) slideIndex = slides.length
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"
     }
-    for(i = 0; i < quadrates.length;i++){
-        quadrates[i].className = quadrates[i].className.replace("active","")
+    for (i = 0; i < quadrates.length; i++) {
+        quadrates[i].className = quadrates[i].className.replace("active", "")
     }
-    slides[slideIndex-1].style.display = "block";
-    quadrates[slideIndex-1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
+    quadrates[slideIndex - 1].className += " active";
 }
 
