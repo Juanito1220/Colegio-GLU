40a6bb531defa2e26d6476d01e20816025c183a8
Juan Pablo Rodriguez <jprodriguez@dcathenea.local>
2023-01-13 17:31:42 -0500

    Admisiones pruebas

---
+++ js/admisiones.js
@@ -0,0 +1,18 @@
+
+document.querySelectorAll(".card").forEach(e => {
+  e.addEventListener("mouseout", es => {
+    const ids = es.target.getAttribute("id");
+   let text_card = document.querySelectorAll(".text_card")
+if(ids){
+switch(ids){
+case 'card1'
+ document.getElementById
+break
+
+}
+}
+
+
+console.log(text_card)
+  });
+});
