5e3d86850b8f8811d1a619b55e1781ab0f070eaf
Juanito1220 <proyectosoftwarejp@gmail.com>
2023-01-17 22:46:55 -0500

    Admision efectos corregidos

--- js/contacto.js
+++ js/contacto.js
@@ -2,8 +2,22 @@ let nombre = document.getElementById("nombre");
 let correo = document.getElementById("correo");
 let telefono = document.getElementById("telefono");
 let mensaje = document.getElementById("input_mensaje");
+let enviar = document.getElementById("envio")
+
 
 
 function ocultarplace() {
     nombre.classList.remove("placeholder")
 }
+
+function mensajeEnvio(){
+
+    if(nombre.value!="" && correo.value!="" && telefono.value!="" && mensaje.value!=""){
+        Swal.fire({
+           title:"Su mensaje se ha enviado de forma exitosa",
+           icon:'success',
+        });
+    }
+}
+
+enviar.addEventListener('click',mensajeEnvio)
