0c47b0b5e130156af672c29043faff29f1d97add
Juan Pablo Rodriguez <jprodriguez@dcathenea.local>
2023-01-20 16:30:25 -0500

    Finalizacion de paginas con response design

--- js/contacto.js
+++ js/contacto.js
@@ -3,7 +3,21 @@ let correo = document.getElementById("correo");
 let telefono = document.getElementById("telefono");
 let mensaje = document.getElementById("input_mensaje");
 let enviar = document.getElementById("envio")
+let elemento = document.getElementById("menu");
+let contenedor = document.getElementById("main");
 
+
+function desactivar() {
+    elemento.checked = false
+}
+
+function activar() {
+    if (elemento.checked == true) {
+        contenedor.classList.add("space");
+    } else {
+        contenedor.classList.remove("space");
+    }
+}
 
 
 function ocultarplace() {
